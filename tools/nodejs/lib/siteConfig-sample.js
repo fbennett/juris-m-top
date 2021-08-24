6ab8e513c56eab556922caa5a46010520be710b5
Frank Bennett <biercenator@gmail.com>
2021-08-24 10:44:48 +0900

    Config sample

---
+++ tools/nodejs/lib/siteConfig-sample.js
@@ -0,0 +1,28 @@
+module.exports = {
+    siteTitle: "The Jurism Project",
+    siteMaintainer: "Frank Bennett",
+    rights: "© 2019 Frank Bennett",
+    repoRoot: '/path/to/repo/dir',
+    templateDir: 'templates',
+    embedsDir: 'templates',
+    buildDir: 'docs',
+    cardTypes: {
+        software: {
+            dirname: "software",
+            name: "Software"
+        },
+        styles: {
+            dirname: "styles",
+            name: "Styles"
+        },
+        "juris-bundles": {
+            dirname: "juris-bundles",
+            name: "Juris Bundles"
+        },
+        links: {
+            dirname: "links",
+            name: "Links"
+        }
+    },
+    urlBase: 'http://localhost:8080'
+}
