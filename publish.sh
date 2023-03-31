0df6449214f1f2a1627c9d7c2d044c846e45dba0
Frank Bennett <biercenator@gmail.com>
2023-03-31 19:51:43 +0900

    Remove stale files

--- publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#git add  build
-git commit -m "Updating site" build
-git subtree push --prefix=build git@github.com:fbennett/juris-m-top.git gh-pages --squash
-#git subtree push --prefix=build git@github.com:fbennett/juris-m-top.git gh-pages
